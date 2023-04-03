50b07f53b4cfba9087415f1e2574950437d9b1c6
tized6 <thirisan-6@hotmail.com>
2023-04-03 10:20:32 +0200

    com

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
-
+import com.example.demo.data.Voiture;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class DemoApplication {
-	pri
+
+	private Logger logger = new Logger();
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	
+
 }
+
+
+	public void run(String... args) throws Exception{
+		voitureRepository.save(new Voiture("Ford","4000000"));
+
+	}
